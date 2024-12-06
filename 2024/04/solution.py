8714796618127cc21d00c0cc95da3e48414af938
Rik <rik.de.kort@hey.com>
2024-12-06 13:24:29 +0100

    5

--- 2024/04/solution.py
+++ 2024/04/solution.py
@@ -1,38 +1,54 @@
+import copy
 from pathlib import Path
 import re
 
 folder = Path(__file__).parent
 
 with open(folder / 'input.txt') as handle:
-    data = [line.strip() for line in handle.readlines()]
+    data = [list(line.strip()) for line in handle.readlines()]
 
 i_max = len(data)
 j_max = len(data[0])
 
-matcher = re.compile('XMAS')
+count = 0
+# Check horizontal
+for i in range(i_max):
+    for j in range(j_max - 3):
+        chars = [data[i][j + k] for k in range(4)]
+        count += ''.join(chars) == 'XMAS' or ''.join(chars) == 'SAMX'
+# Check vertical
+for i in range(i_max - 3):
+    for j in range(j_max):
+        chars = [data[i + k][j] for k in range(4)]
+        count += ''.join(chars) == 'XMAS' or ''.join(chars) == 'SAMX'
+# Check diagonal top left to bottom right
+for i in range(i_max - 3):
+    for j in range(j_max - 3):
+        chars = [data[i + k][j + k] for k in range(4)]
+        count += ''.join(chars) == 'XMAS' or ''.join(chars) == 'SAMX'
+# Check diagonal top right to bottom left
+for i in range(i_max - 3):
+    for j in range(3, j_max):
+        chars = [data[i + k][j - k] for k in range(4)]
+        count += ''.join(chars) == 'XMAS' or ''.join(chars) == 'SAMX'
+print(count)
 
-def find_matches(data):
-    return sum(len(matcher.findall(line)) for line in data)
+
+def print_all(items):
+    print('\n'.join(''.join(line) for line in items))
 
 
-print('\n'.join(data))
-print(find_matches(data))
-print('-'*15)
-
-reversed =[ ''.join(reversed(line)) for line in data]
-print('\n'.join(reversed))
-print(find_matches(reversed))
-
-transposed = [''.join(line) for line in zip(*data)]
-print('\n'.join(transposed))
-print(find_matches(transposed))
-
-# # print(sum(len(matcher.findall(line)) for line in data))
-# # print(sum(len(matcher.findall(''.join(reversed(line)))) for line in data))
-# # diagonals = [''.join(data[i+k][j+k] for k in range(min(i_max-i-1, j_max-j-1))) for i in range(0, i_max) for j in range(0, j_max)]
-# # print(sum(len(matcher.findall(line)) for line in diagonals))
-# # print(sum(len(matcher.findall(''.join(reversed(line)))) for line in diagonals))
-# #
-#
-# transposed = [''.join(line) for line in zip(*data)]
-#
+count = 0
+for i in range(1, i_max - 1):
+    for j in range(1, j_max - 1):
+        if data[i][j] != 'A': continue
+        tl = data[i-1][j-1]
+        tr = data[i-1][j+1]
+        bl = data[i+1][j-1]
+        br = data[i+1][j+1]
+        count += ((tl == 'S' and br == 'M') or (tl == 'M' and br == 'S')) and ((tr == 'S' and bl == 'M') or (tr == 'M' and bl == 'S'))
+        # tl_br = data[i-1][j-1] + data[i][j] + data[i+1][j+1]
+        # tr_bl = data[i-1][j+1] + data[i][j] + data[i+1][j-1]
+        # print((i, j), tl_br, tr_bl)
+        # count += (tl_br == 'MAS' or tl_br == 'SAM') and (tr_bl == 'MAS' or tl_br == 'SAM')
+print(count)
