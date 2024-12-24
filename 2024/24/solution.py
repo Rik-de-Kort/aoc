83b41d788c60d49e8969f1cf63e4043c5ca2ab53
Rik de Kort <rik.de.kort@pggm.nl>
2024-12-24 13:56:42 +0100

    24 pt1.5

--- 2024/24/solution.py
+++ 2024/24/solution.py
@@ -77,3 +77,73 @@ def execute(instructions, inputs):
 
 
 print(to_int(execute(deepcopy(instructions), deepcopy(inputs)), lambda k: k.startswith('z')))
+
+
+def x(i: int) -> str:
+    return f'x{i:02}'
+
+
+def y(i: int) -> str:
+    return f'y{i:02}'
+
+
+# find inputs that depend on xi
+def find_depending_on(var: str, instructions: list[Instruction]) -> set[Instruction]:
+    direct = {i for i in instructions if i.x == var or i.y == var}
+    return direct | {dep for i in direct for dep in find_depending_on(i.out, instructions)}
+
+
+def find_ancestors(var: str, instructions: list[Instruction]) -> set[Instruction]:
+    direct = {i for i in instructions if i.out == var}
+    return (direct
+            | {anc for i in direct for anc in find_ancestors(i.x, instructions)}
+            | {anc for i in direct for anc in find_ancestors(i.y, instructions)})
+
+
+def find_broken_first_broken(inputs, instructions) -> Optional[int]:
+    instructions = deepcopy(instructions)
+
+    original_inputs = inputs
+    inputs = deepcopy(inputs)
+
+    i_max = max(int(s.replace('x', '').replace('y', '')) for s in inputs.keys()) + 1
+    for i in range(1, i_max):
+        inputs[x(i)] = 0
+        inputs[y(i)] = 0
+
+    for i in range(1, i_max):
+        x_ = to_int(inputs, lambda x: x.startswith('x'))
+        y_ = to_int(inputs, lambda y: y.startswith('y'))
+        expected = to_bits(x_ + y_, 'z', i_max + 1)
+        result = execute(deepcopy(instructions), inputs)
+        result_keys = sorted([k for k in result.keys() if k.startswith('z')])
+        for k0, k1 in pairwise(result_keys):
+            if result[k1] != expected[k1]:
+                candidates = find_ancestors(k0, instructions) - find_ancestors(k1, instructions)
+                print(candidates)
+                return k0, k1
+        inputs[x(i)] = original_inputs[x(i)]
+        inputs[y(i)] = original_inputs[y(i)]
+
+
+swapped = []
+cnt = 0
+while True:
+    i = find_broken_first_broken(inputs, instructions)
+    print(cnt, i)
+    if i is None: break
+
+    d_x0 = find_depending_on(x(i), instructions)
+    d_y0 = find_depending_on(y(i), instructions)
+    d_x1 = find_depending_on(x(i - 1), instructions)
+    d_y1 = find_depending_on(y(i - 1), instructions)
+    borked = list((d_x0 & d_y0) - d_x1 - d_y1)
+    swapped.append(borked)
+    assert len(borked) == 2
+
+    out0, out1 = deepcopy(borked[0].out), deepcopy(borked[1].out)
+    instructions[borked[0].i].out = out1
+    instructions[borked[1].i].out = out0
+    cnt += 1
+
+print(borked)
